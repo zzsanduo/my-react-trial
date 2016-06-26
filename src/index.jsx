6097ddc25179c6b6f2c955c4792b6745bf0f7578
zzsanduo <zzsanduo@gmail.com>
2016-06-26 22:30:55 +0800

    add props to component

--- src/index.jsx
+++ src/index.jsx
@@ -6,10 +6,11 @@ class App extends React.Component {
  render () {
     return (
       <div>
-        <p> Hello World!</p>
+        <p> Hello World!  I am {this.props.name}</p>
+        <p>Now :{this.props.now}</p>
       </div>
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App name="ZHAO Yan" now={new Date().toString()}/>, document.getElementById('app'));
