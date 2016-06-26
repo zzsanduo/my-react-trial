4ce8085620dac4adcce3fb68a3dc2b5b371000d0
zzsanduo <zzsanduo@gmail.com>
2016-06-26 23:14:28 +0800

    add state

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,27 @@ import {render} from 'react-dom';
 import Inner from './inner.jsx';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        };
+        this.handleCounter = this.handleCounter.bind(this);
+    }
 
- render () {
-    return (
-      <div>
-        <p> Hello World!  I am {this.props.name}</p>
-        <Inner now={new Date().toString()} />
-      </div>
-    );
-  }
+    handleCounter () {
+        this.setState({count:this.state.count + 1 });
+    }
+
+    render () {
+        return (
+          <div>
+            <p> Hello World!  I am {this.props.name}</p>
+            <button onClick={this.handleCounter}>Click me</button>
+            <Inner now={new Date().toString()} count={this.state.count}/>
+          </div>
+        );
+    }
 }
 
 render(<App name="ZHAO Yan" />, document.getElementById('app'));
--- src/inner.jsx
+++ src/inner.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class Inner extends React.Component {
+    
     render () {
         return <div>
-         <p>Now :{this.props.now}</p>
+            <p> Count : {this.props.count}</p>
+         <p>Now : {this.props.now}</p>
         </div>
     }
 }
 
 Inner.propTypes = {
+    count: React.PropTypes.number.isRequired,
     now: React.PropTypes.string.isRequired
 };
 
